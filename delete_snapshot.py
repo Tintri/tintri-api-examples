e6c6e2f998297a929a7ddc25137a0d35072b3be3
Rick Ehrhart <rick_ehrhart@yahoo.com>
2015-01-26 18:55:08 -0800

    Update delete_snapshot.py
    Removed home-brewed license and added MIT license.

--- delete_snapshot.py
+++ delete_snapshot.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env/python
 # -*- coding: utf-8 -*-
 #
-# The code example provided here is for reference only to illustrate
-# sample workflows and may not be appropriate for use in actual operating
-# environments.
-# Support will be provided by Tintri for the Tintri APIs, but theses
-# examples are for illustrative purposes only and are not supported.
-# Tintri is not responsible for any outcome resulting for the use
-# of these scripts.
+# The MIT License (MIT)
 #
+# Copyright (c) 2015 Tintri, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 import requests
 import json
