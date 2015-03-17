0afa204f9d07dba7b97dba854f46aa9a1e183235
Rick Ehrhart <rick_ehrhart@yahoo.com>
2015-03-17 15:23:11 -0700

    Get VMs
    Show how to get all the VMs.

---
+++ get_vms.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env/python
+# -*- coding: utf-8 -*-
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 Tintri, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import json
+import sys
+import tintri
+
+"""
+ This Python script prints the VM name and UUID for each VM.
+
+ Command usage: get_vms <server_name> <userName> <password>
+
+"""
+
+# For exhaustive messages on console, make it to True; otherwise keep it False
+debug_mode = False
+
+
+def print_with_prefix(prefix, out):
+    print(prefix + out)
+    return
+
+
+def print_debug(out):
+    if debug_mode:
+        print_with_prefix("[DEBUG] : ", out)
+    return
+
+
+def print_info(out):
+    print_with_prefix("[INFO] : ", out)
+    return
+
+
+def print_error(out):
+    print_with_prefix("[ERROR] : ", out)
+    return
+
+
+# main
+if len(sys.argv) < 4:
+    print("\nPrints VM information\n")
+    print("Usage: " + sys.argv[0] + " server_name user_name password\n")
+    sys.exit(-1)
+
+server_name = sys.argv[1]
+user_name = sys.argv[2]
+password = sys.argv[3]
+
+# Could make this an input parameter
+# Get the preferred version
+r = tintri.api_get(server_name, '/info')
+json_info = r.json()
+
+print_info("API Version: " + json_info['preferredVersion'])
+
+# Login to VMstore
+session_id = tintri.api_login(server_name, user_name, password)
+
+# Get a list of VMs, but only return a page size
+url = "/v310/vm"
+r = tintri.api_get(server_name, url, session_id)
+print_debug("The JSON response of the get invoke to the server " +
+            server_name + " is: " + r.text)
+
+# if HTTP Response is not 200 then raise an error
+if r.status_code != 200:
+    print_error("The HTTP response for the get invoke to the server " +
+          server_name + " is not 200, but is: " + str(r.status_code))
+    print_error("url = " + url)
+    print_error("response: " + r.text)
+    tintri.api_logout(server_name, session_id)
+    sys.exit(-10)
+
+vm_paginated_result = r.json()
+num_vms = int(vm_paginated_result["filteredTotal"])
+if num_vms == 0:
+    print_error("No VMs present")
+    tintri.api_logout(server_name, session_id)
+    exit(88)
+
+print_info(str(num_vms) + " VMs present")
+
+# For each VM, print the VM name and UUID
+items = vm_paginated_result["items"]
+count = 1
+for vm in items:
+    vm_name = vm["vmware"]["name"]
+    vm_uuid = vm["uuid"]["uuid"]
+    print(str(count) + ": " + vm_name + ", " + vm_uuid)
+    count += 1
+
+# All pau, log out
+tintri.api_logout(server_name, session_id)
+
