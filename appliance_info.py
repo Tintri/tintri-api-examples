13523ee78e06bd346ca3161c7eb66bedf18b1131
Rick Ehrhart <rick_ehrhart@yahoo.com>
2016-08-01 13:27:51 -0700

    Display VMstore appliance information
    Added code to display more VMstore appliance information.  The previous
    version is now appliance_status.py

--- appliance_info.py
+++ appliance_info.py
@@ -77,6 +77,8 @@ try:
     r = tintri.api_version(server_name)
     json_info = r.json()
     product_name = json_info['productName']
+    if json_info['productName'] != "Tintri VMstore":
+        raise tintri.TintriRequestException("server needs to be a VMstore.")
 
     # Login to Tintri server
     session_id = tintri.api_login(server_name, user_name, password)
@@ -89,11 +91,25 @@ except tintri.TintriApiException as tae:
     exit(-3)
     
 try:
+    print("\nLoading transporter buffers\n")
+
     # Get appliance info
-    url = "/v310/appliance/default/info"
+    url = "/v310/appliance"
     r = tintri.api_get(server_name, url, session_id)
     print_debug("The JSON response of the get invoke to the server " +
                 server_name + " is: " + r.text)
+
+    appliance_resp = r.json()
+    appliance = appliance_resp[0]
+
+    # Get failed Components for the appliance
+    url = "/v310/appliance/default/failedComponents"
+    r = tintri.api_get(server_name, url, session_id)
+    print_debug("The JSON response of the get invoke to the server " +
+                server_name + " is: " + r.text)
+
+    failed_components_resp = r.json()
+    failed_components = failed_components_resp['failedComponents']
 
 except tintri.TintriRequestsException as tre:
     print_error(tre.__str__())
@@ -107,17 +123,17 @@ except tintri.TintriApiException as tae:
 # log out
 tintri.api_logout(server_name, session_id)
 
-# Some OS versions do not return all flash.
+# Some basic info
 all_flash = False
 show_all_flash = False
 
-appliance_info = r.json()
+appliance_info = appliance['info']
+
 if 'isAllFlash' in appliance_info:
     all_flash = appliance_info['isAllFlash']
     show_all_flash = True
 
-print("")
-
+print("Appliance")
 table_header = ('Info', 'Value')
 table = PrettyTable(table_header)
 table.align['Info'] = "l"
@@ -133,7 +149,10 @@ if show_all_flash:
     row = ('All Flash', all_flash)
     table.add_row(row)
 
-row = ('OS version', appliance_info['osVersion'])
+long_os_version = appliance_info['osVersion']
+dash_x = long_os_version.index("-")
+os_version = long_os_version[0:dash_x]
+row = ('OS version', os_version)
 table.add_row(row)
 
 row = ('API version', json_info['preferredVersion'])
@@ -142,3 +161,108 @@ table.add_row(row)
 print(table)
 print("")
 
+# Appliance Component info
+print("Appliance Componets")
+table_header = ('Component', 'Status', 'Location')
+table = PrettyTable(table_header)
+table.align['Component'] = "l"
+table.align['Status'] = "l"
+table.align['Location'] = "l"
+
+components = appliance['components']
+for component in components:
+    row = (component['type'], component['status'], component['locator'])
+    table.add_row(row)
+
+print(table)
+print("")
+
+# Show failed components
+if (len(failed_components) == 0):
+    print("No failed components")
+else:
+    print("Failed Components")
+    table_header = ('Component', 'Serial #', 'Description')
+    table = PrettyTable(table_header)
+    table.align['Component'] = "l"
+    table.align['Serial #'] = "l"
+    table.align['Description'] = "l"
+    for component in failed_components:
+       row = (component['componentType'], component['serialNumber'], component['description'])
+       table.add_row(row)
+    
+    print(table)
+print("") 
+    
+# Show the configured IP address information
+table_header = ('IP', 'Service Type', 'Network Bond', 'VLAN ID')
+table = PrettyTable(table_header)
+table.align['IP'] = 'l'
+
+ip_configs = appliance['configIps']
+for ip_config in ip_configs:
+    row = (ip_config['ip'], ip_config['serviceType'], ip_config['networkBond'], ip_config['vlanId'])
+    table.add_row(row)
+
+print(table)
+print ""
+
+# Now show each controller information
+table_header = ('Component', 'Location', 'Status')
+nb_table_hdr = ('Port', 'Port Status', 'Role', 'Speed')
+
+# Pull the controller information
+controllers = appliance['controllers']
+for controller in controllers:
+    print(controller['locator'] + ": " + controller['state'] + " - " + controller['role'])
+    table = PrettyTable(table_header)
+    table.align['Component'] = "l"
+    table.align['Location'] = "l"
+    table.align['Status'] = "l"
+
+    components = controller['components']
+    for component in components:
+        row = (component['type'], component['locator'], component['status'])
+        table.add_row(row)
+
+    print(table)
+    print("")
+
+    # Add network information
+    network_bonds = controller['networkBonds']
+    for nb in network_bonds:
+        print(controller['locator'] + ": " + nb['name'] + ": " + nb['type'] + ": " + nb['status'] + ": " + nb['macAddress'])
+        table = PrettyTable(nb_table_hdr)
+        for port in nb['ports']:
+            port_speed = str(port['maxSpeed']) + port['maxUnit']
+            nb_row = (port['locator'], port['status'], port['role'], port_speed)
+            table.add_row(nb_row)
+        print(table)
+
+    print("")
+
+# Disks
+if (not 'disks' in appliance):
+    print("No disk information present")
+    sys.exit(0)
+
+print("Disks")
+table_header = ('Name', 'Status', 'Type')
+table = PrettyTable(table_header)
+table.align['Name'] = "l"
+table.align['Status'] = "l"
+table.align['Type'] = "l"
+disks = appliance['disks']
+for disk in disks:
+    if (disk['state'] == "DISK_STATE_REBUILD"):
+        disk_state = disk['state'] + " (" + str(disk['rebuildPercent']) + "%)"
+    else:
+        disk_state = disk['state']
+    if 'diskType' in disk:
+        row = (disk['locator'], disk_state, disk['diskType'])
+    else:
+        row = (disk['locator'], disk_state, disk['type'])
+    table.add_row(row)
+
+print(table)
+print("")
